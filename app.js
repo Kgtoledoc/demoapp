0abb897829b04799b5c57c85f5d288741b7aaaea
Kevin Toledo <kgtoledoc@unal.edu.co>
2018-10-20 17:38:22 -0500

    Code for upload images!

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+const fileUpload = require('express-fileupload');
 const fs = require('fs');
+const formidable = require('express-formidable');
 const responseRoutes = require('./controllers/response.routes');
 const baseRoutes = require('./controllers/base.routes');
 const uploadRoutes = require('./controllers/upload.routes');
@@ -12,14 +14,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
    extended: true,
  }));
+app.use(fileUpload());
 
-app.use(express.static('public'));
+app.use("/public", express.static('public'));
 app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', baseRoutes);
 app.use('/upload', uploadRoutes);
 app.use('/response', responseRoutes);
+
 
 
 app.listen(3000, (err) => {
--- controllers/upload.routes.js
+++ controllers/upload.routes.js
@@ -2,8 +2,31 @@
 
 const Router = require('express').Router();
 
+
 Router.get('/', (req, res) => {
   res.render('upload',{});
 })
+Router.post('/finish', (req, res) => {
+  console.log(req.files.archivo)
+  let imagen = req.files.archivo
+  imagen.mv(`./public/image.jpg`, err => {
+    if(err){
+      return res.status(500).send({ message: err});
+    }
+    return res.status(200).render('finish',{});
+  })
+})
 
+/*Router.post('/finish', (req, res) => {
+  let imagen = req.files
+  console.log(req.body);
+  imagen.mv(`./public/${imagen.name}`, err => {
+    if(err){
+      return res.status(500).send({ message: err});
+    } else {
+      return res.status(200).send({ message: 'File uploaded'});
+    }
+  })
+})
+*/
 module.exports = Router;
